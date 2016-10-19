474e171b860c9ca2e36e729be43a395f3624ff50
Leonard Loo <leonard.loo.ks@gmail.com>
2016-10-18 23:12:30 -0400

    Keyboard on EditProfession

--- ios-428-app/ios-428-app/EditProfessionalController.swift
+++ ios-428-app/ios-428-app/EditProfessionalController.swift
@@ -29,6 +29,26 @@ class EditProfessionalController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        
+        self.automaticallyAdjustsScrollViewInsets = false
+        self.setupViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.registerObservers()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unregisterObservers()
+    }
+    
+    // MARK: Views
+    
     fileprivate func titleLabelTemplate() -> UILabel {
         let label = UILabel()
         label.font = FONT_HEAVY_LARGE
@@ -69,9 +89,9 @@ class EditProfessionalController: UIViewController, UITextFieldDelegate {
         let editImageView = UIImageView(image: #imageLiteral(resourceName: "edit"))
         editImageView.frame = CGRect(x: 0, y: 0, width: editImageView.image!.size.width + 20.0, height: editImageView.image!.size.height)
         editImageView.contentMode = .center
-        
         textfield.rightView = editImageView
         textfield.rightViewMode = .unlessEditing
+        textfield.returnKeyType = .done
         return textfield
     }
     
@@ -93,14 +113,6 @@ class EditProfessionalController: UIViewController, UITextFieldDelegate {
         return textfield
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = UIColor.white
-        self.automaticallyAdjustsScrollViewInsets = false
-        
-        self.setupViews()
-    }
-    
     fileprivate func setupViews() {
         view.addSubview(orgTitleLabel)
         view.addSubview(orgTextField)
@@ -118,6 +130,73 @@ class EditProfessionalController: UIViewController, UITextFieldDelegate {
         
         view.addConstraint(NSLayoutConstraint(item: orgTitleLabel, attribute: .top, relatedBy: .equal, toItem: topLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 15.0))
         view.addConstraintsWithFormat("V:[v0(20)]-5-[v1(45)]-25-[v2(20)]-5-[v3(45)]-25-[v4(20)]-5-[v5(45)]", views: orgTitleLabel, orgTextField, schoolTitleLabel, schoolTextField, disciplineTitleLabel, disciplineTextField)
+    }
+    
+    // Mark: Text field
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        // Max number of characters is 50
+        let maxLength = 50
+        let nsString = textField.text as NSString?
+        let newString = nsString?.replacingCharacters(in: range, with: string)
+        if let newLength = newString?.characters.count {
+            return newLength <= maxLength
+        }
+        return false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    // MARK: Move keyboard up if blocking
+    
+    fileprivate func registerObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    fileprivate func unregisterObservers() {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    fileprivate func getActiveTextfield() -> UITextField? {
+        if self.orgTextField.isFirstResponder {
+            return self.orgTextField
+        }
+        if self.schoolTextField.isFirstResponder {
+            return schoolTextField
+        }
+        if self.disciplineTextField.isFirstResponder {
+            return disciplineTextField
+        }
+        return nil
+    }
+    
+    func handleKeyboardNotification(_ notification: Notification) {
+        if let userInfo = notification.userInfo, let keyboardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue, let animationDuration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval {
+            
+            let isKeyboardShowing = notification.name != Notification.Name.UIKeyboardWillHide
+            let keyboardViewEndFrame = view.convert(keyboardFrame, from: view.window)
+            let keyboardHeight = keyboardViewEndFrame.height
+            guard let activeTextField = getActiveTextfield() else {
+                return
+            }
+            let bottomOfView = activeTextField.frame.maxY
+            let topOfKeyboard = self.view.bounds.maxY - keyboardHeight
+            
+            UIView.animate(withDuration: animationDuration, animations: { 
+                if isKeyboardShowing && bottomOfView >= topOfKeyboard {
+                    let distanceShifted = min(keyboardHeight, abs(bottomOfView - topOfKeyboard))
+                    self.view.frame.origin.y = -distanceShifted
+                } else {
+                    self.view.frame.origin.y = 0
+                }
+            })
+        }
     }
 }
 
