386d68e5c07ec7e34611da495de11604a812e75b
Leonard Loo <leonard.loo.ks@gmail.com>
2016-12-22 18:17:24 +0800

    EditTaglineController view done

--- ios-428-app/ios-428-app/EditTaglineController.swift
+++ ios-428-app/ios-428-app/EditTaglineController.swift
@@ -14,21 +14,14 @@ class EditTaglineController: UIViewController, UITextViewDelegate {
     
     fileprivate let MAX_CHARACTERS = 400
     
-    var tagline1: String? {
+    var tagline: String? {
         didSet {
-            self.tagline1TextView.text = tagline1
-            self.tagline1CountLabel.text = "\(MAX_CHARACTERS - tagline1!.characters.count)"
-            self.tagline1Placeholder.isHidden = tagline1 != ""
+            self.taglineTextView.text = tagline
+            self.taglineCountLabel.text = "\(MAX_CHARACTERS - tagline!.characters.count)"
+            self.taglinePlaceholder.isHidden = tagline != ""
         }
     }
     
-    var tagline2: String? {
-        didSet {
-            self.tagline2TextView.text = tagline2
-            self.tagline2CountLabel.text = "\(MAX_CHARACTERS - tagline2!.characters.count)"
-            self.tagline2Placeholder.isHidden = tagline2 != ""
-        }
-    }
     
     fileprivate lazy var saveButton: UIBarButtonItem = {
         let button: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.saveEdits))
@@ -37,15 +30,13 @@ class EditTaglineController: UIViewController, UITextViewDelegate {
     }()
     
     func saveEdits() {
-        let tagline1Saved = tagline1TextView.text.trim().lowercaseFirstLetter()
-        let tagline2Saved = tagline2TextView.text.trim().lowercaseFirstLetter()
+        let taglineSaved = taglineTextView.text.trim().lowercaseFirstLetter()
         if myProfile != nil {
-            myProfile!.tagline1 = tagline1Saved
-            myProfile!.tagline2 = tagline2Saved
+            myProfile!.tagline1 = taglineSaved
             NotificationCenter.default.post(name: NOTIF_MYPROFILEDOWNLOADED, object: nil)
         }
         
-        DataService.ds.updateUserFields(tagline1: tagline1Saved, tagline2: tagline2Saved) { (isSuccess) in
+        DataService.ds.updateUserFields(tagline1: taglineSaved) { (isSuccess) in
             if !isSuccess {
                 log.error("[Error] Taglines failed to be updated")
             }
@@ -84,35 +75,23 @@ class EditTaglineController: UIViewController, UITextViewDelegate {
         guard let profile = myProfile else {
             return
         }
-        self.tagline1 = profile.tagline1
-        self.tagline2 = profile.tagline2
+        self.tagline = profile.tagline1
     }
     
     // MARK: Views
     
-    fileprivate func placeholderTemplate() -> UILabel {
+    fileprivate lazy var taglinePlaceholder: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
         label.font = UIFont.systemFont(ofSize: 16.0)
         label.textColor = UIColor.lightGray
         label.sizeToFit()
         label.isHidden = true
-        return label
-    }
-    
-    fileprivate lazy var tagline1Placeholder: UILabel = {
-        let label: UILabel = self.placeholderTemplate()
-        label.text = "the app we call 428..."
+        label.text = "At 4:28pm, you can find me..."
         return label
     }()
     
-    fileprivate lazy var tagline2Placeholder: UILabel = {
-        let label: UILabel = self.placeholderTemplate()
-        label.text = "make my mark on the world..."
-        return label
-    }()
-    
-    fileprivate func textViewTemplate() -> UITextView {
+    fileprivate lazy var taglineTextView: UITextView = {
         let textView = UITextView()
         textView.font = UIFont.systemFont(ofSize: 16.0)
         textView.textColor = UIColor.darkGray
@@ -122,96 +101,52 @@ class EditTaglineController: UIViewController, UITextViewDelegate {
         textView.textContainerInset = UIEdgeInsetsMake(5.0, 5.0, 5.0, 5.0)
         textView.tintColor = GREEN_UICOLOR
         return textView
-    }
-    
-    fileprivate lazy var tagline1TextView: UITextView = {
-        let textView: UITextView = self.textViewTemplate()
-        return textView
     }()
     
-    fileprivate lazy var tagline2TextView: UITextView = {
-        let textView: UITextView = self.textViewTemplate()
-        return textView
-    }()
-    
-    fileprivate func labelTemplate() -> UILabel {
+    fileprivate lazy var taglineLabel: UILabel = {
         let label = UILabel()
         label.font = FONT_HEAVY_LARGE
-        label.textColor = GREEN_UICOLOR
+        label.textColor = UIColor.black
         label.textAlignment = .left
-        return label
-    }
-    
-    fileprivate lazy var tagline1Label: UILabel = {
-        let label: UILabel = self.labelTemplate()
-        label.text = "I am working on..."
+        label.text = "What I do at 4:28pm:"
         return label
     }()
     
-    fileprivate lazy var tagline2Label: UILabel = {
-        let label: UILabel = self.labelTemplate()
-        label.text = "I want to eventually..."
-        return label
-    }()
-    
-    fileprivate func countLabelTemplate() -> PaddingLabel {
+    fileprivate lazy var taglineCountLabel: UILabel = {
         let label = PaddingLabel()
         label.font = FONT_HEAVY_SMALL
         label.textColor = UIColor.gray
         label.textAlignment = .right
         label.backgroundColor = UIColor.white
         return label
-    }
-    
-    fileprivate lazy var tagline1CountLabel: UILabel = {
-        let label: UILabel = self.countLabelTemplate()
-        return label
-    }()
-    
-    fileprivate lazy var tagline2CountLabel: UILabel = {
-        let label: UILabel = self.countLabelTemplate()
-        return label
     }()
     
     fileprivate func setupViews() {
-        view.addSubview(tagline1Label)
-        view.addSubview(tagline1TextView)
-        view.addSubview(tagline2Label)
-        view.addSubview(tagline2TextView)
-        view.addSubview(tagline1Placeholder)
-        view.addSubview(tagline2Placeholder)
-        view.addSubview(tagline1CountLabel)
-        view.addSubview(tagline2CountLabel)
+        view.addSubview(taglineLabel)
+        view.addSubview(taglineTextView)
+        view.addSubview(taglinePlaceholder)
+        view.addSubview(taglineCountLabel)
         
-        view.addConstraintsWithFormat("H:|-12-[v0]", views: tagline1Label)
-        view.addConstraintsWithFormat("H:|-12-[v0]-12-|", views: tagline1TextView)
-        view.addConstraintsWithFormat("H:|-12-[v0]", views: tagline2Label)
-        view.addConstraintsWithFormat("H:|-12-[v0]-12-|", views: tagline2TextView)
-        view.addConstraintsWithFormat("H:|-12-[v0]-12-|", views: tagline1CountLabel)
-        view.addConstraintsWithFormat("H:|-12-[v0]-12-|", views: tagline2CountLabel)
+        view.addConstraintsWithFormat("H:|-12-[v0]", views: taglineLabel)
+        view.addConstraintsWithFormat("H:|-12-[v0]-12-|", views: taglineTextView)
+        view.addConstraintsWithFormat("H:|-12-[v0]-12-|", views: taglineCountLabel)
         
-        view.addConstraint(NSLayoutConstraint(item: tagline1Label, attribute: .top, relatedBy: .equal, toItem: topLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 12.0))
+        view.addConstraint(NSLayoutConstraint(item: taglineLabel, attribute: .top, relatedBy: .equal, toItem: topLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 12.0))
         
-        // Each text view is 0.3 * screenSize
-        let textviewHeight = 0.28 * UIScreen.main.bounds.height
-        view.addConstraintsWithFormat("V:[v0(20)]-8-[v1(\(textviewHeight))][v2(20)]-20-[v3(20)]-8-[v4(\(textviewHeight))][v5(20)]", views: tagline1Label, tagline1TextView, tagline1CountLabel, tagline2Label, tagline2TextView, tagline2CountLabel)
+        let textviewHeight = 0.33 * UIScreen.main.bounds.height
+        view.addConstraintsWithFormat("V:[v0(20)]-8-[v1(\(textviewHeight))][v2(20)]", views: taglineLabel, taglineTextView, taglineCountLabel)
         
         // Align placeholders to text views
-        view.addConstraintsWithFormat("H:|-23-[v0]-13-|", views: tagline1Placeholder)
-        view.addConstraintsWithFormat("H:|-23-[v0]-13-|", views: tagline2Placeholder)
-        view.addConstraintsWithFormat("V:[v0(100)]", views: tagline1Placeholder)
-        view.addConstraintsWithFormat("V:[v0(100)]", views: tagline2Placeholder)
+        view.addConstraintsWithFormat("H:|-23-[v0]-13-|", views: taglinePlaceholder)
+        view.addConstraintsWithFormat("V:[v0(100)]", views: taglinePlaceholder)
         
-        view.addConstraint(NSLayoutConstraint(item: tagline1Placeholder, attribute: .top, relatedBy: .equal, toItem: tagline1Label, attribute: .bottom, multiplier: 1.0, constant: -24.0))
+        view.addConstraint(NSLayoutConstraint(item: taglinePlaceholder, attribute: .top, relatedBy: .equal, toItem: taglineLabel, attribute: .bottom, multiplier: 1.0, constant: -28.0))
         
-        view.addConstraint(NSLayoutConstraint(item: tagline2Placeholder, attribute: .top, relatedBy: .equal, toItem: tagline2Label, attribute: .bottom, multiplier: 1.0, constant: -24.0))
     }
     
     func textViewDidChange(_ textView: UITextView) {
-        if textView == tagline1TextView {
-            self.tagline1Placeholder.isHidden = !textView.text.isEmpty
-        } else {
-            self.tagline2Placeholder.isHidden = !textView.text.isEmpty
+        if textView == taglineTextView {
+            self.taglinePlaceholder.isHidden = !textView.text.isEmpty
         }
     }
     
@@ -220,18 +155,9 @@ class EditTaglineController: UIViewController, UITextViewDelegate {
         let numberOfChars = newText.characters.count
         
         if numberOfChars <= MAX_CHARACTERS {
-            if textView == tagline1TextView {
-                tagline1CountLabel.text = "\(max(MAX_CHARACTERS - numberOfChars, 0))"
-            } else {
-                tagline2CountLabel.text = "\(max(MAX_CHARACTERS - numberOfChars, 0))"
-            }
+            taglineCountLabel.text = "\(max(MAX_CHARACTERS - numberOfChars, 0))"
         }
-        
-        if textView == tagline1TextView {
-            saveButton.isEnabled = numberOfChars > 0 && newText != tagline1
-        } else {
-            saveButton.isEnabled = numberOfChars > 0 && newText != tagline2
-        }
+        saveButton.isEnabled = numberOfChars > 0 && newText != tagline
         return numberOfChars < MAX_CHARACTERS
     }
     
@@ -252,11 +178,8 @@ class EditTaglineController: UIViewController, UITextViewDelegate {
     }
     
     fileprivate func getActiveTextView() -> UITextView? {
-        if self.tagline1TextView.isFirstResponder {
-            return self.tagline1TextView
-        }
-        if self.tagline2TextView.isFirstResponder {
-            return self.tagline2TextView
+        if self.taglineTextView.isFirstResponder {
+            return self.taglineTextView
         }
         return nil
     }
